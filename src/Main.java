9a413ee9b258f8efe1a496ee311a921e2d3c15cf
Tonny <tonnygol101@gmail.com>
2024-02-05 17:44:53 +0200

    Initial commit, Starting point, from here we will start to add our code.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Starting point
+        // We will write the code
+        // all together
+    }
+
+    // TO DO: funcEx1, funcEx2, funcEx3
+    //        funcEx4, funcEx5, funcEx6
+    //        funcEx7, funcEx8, funcEx9
+    //        funcEx10, funcEx11, funcEx12
+
+}
