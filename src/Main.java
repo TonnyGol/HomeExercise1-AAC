dc721d40bc884f6419cd6c3861b2e3f3459f8cb2
Nikobro1 <Nikxdb@yahoo.com>
2024-02-08 20:59:40 +0200

    Check func 4,5,6 (V)! Finish all functions

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,9 @@ public class Main {
                 case 1 -> calculate_Circle_AreaAndPerimeter();
                 case 2 -> convertBetween_CelsiusOrFahrenheit();
                 case 3 ->factorialCalculation();
-                case 4 -> System.out.println("Not Working Yet.");
-                case 5 -> System.out.println("Not Working Yet.");
-                case 6 -> System.out.println("Not Working Yet.");
+                case 4 -> naturalNumbers();
+                case 5 -> prime();
+                case 6 -> palindromeTest();
                 case 7 -> printAllPrimeNumbersInRange();
                 case 8 -> print2NumbersGCD();
                 case 9 -> printQuadraticEquationCalculation();
@@ -24,11 +24,7 @@ public class Main {
             }
         }while (userInput != 0);
     }
-    // TO DO: Check Functions 4,5,6
-
-    // Checked Functions 1,2,3 / 7,8,9 / 10,11,12 (Names, functionality, Duplicate code....)
-    // Added function doPowCalculation() to EX12
-
+    // Checked Functions 4,5,6 (Names, functionality, Duplicate code....)
     public static int getIntInput(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter a number: ");
@@ -104,90 +100,70 @@ public class Main {
 
     //------------------------------------------------------------------------------------------------------------------
 
-    public static void naturalNumbers(int naturalNumber1, int naturalNumber2) {
-        Scanner scanner = new Scanner(System.in);
+    public static void naturalNumbers() {
+        System.out.println("Two numbers required");
+        int naturalNumber1 = getIntInput();
+        int naturalNumber2 = getIntInput();
         int sum = 0;
+        int temp;
         while (naturalNumber1 == naturalNumber2) {
             System.out.println("Error, Please enter a new number");
-            naturalNumber1 = scanner.nextInt();
-            naturalNumber2 = scanner.nextInt();
+            naturalNumber1 = getIntInput();
+            naturalNumber2 = getIntInput();
         }
-        for (int i = naturalNumber1; i <= naturalNumber2; i++) {             // does not print if I give the function 10, 5
+        if (naturalNumber1 > naturalNumber2) {
+            temp = naturalNumber1;
+            naturalNumber1 = naturalNumber2;
+            naturalNumber2 = temp;
+        }
+        for (int i = naturalNumber1; i <= naturalNumber2; i++) {
             sum = sum + i;
         }
         System.out.println(sum);
     }
-    public static boolean prime(int firstNumber) {
-        Scanner scanner = new Scanner(System.in);
+    //------------------------------------------------------------------------------------------------------------------
+    public static void prime() {
+        System.out.println("Prime number check");
         boolean prime = true;
-        int count = firstNumber;
-        while (count == 1) {
-            System.out.println("This number is not prime or prime please enter another number");
-            int primeNum = scanner.nextInt();
-            if (primeNum > 1) {
-                count++;
+        int primeNum = getIntInput();
+        while (primeNum <= 1) {
+            System.out.println("This number is not prime enter another number");
+            primeNum = getIntInput();
+        }
+        for (int j = 2; j < primeNum; j++) {
+            if (primeNum % j == 0) {
+                prime = false;
+                break;
             }
         }
-        int primeNum2 = count;
-        if (primeNum2 > 1) {
-            for (int j = 2; j < primeNum2; j++) {
-                if (primeNum2 % j == 0) {
-                    prime = false;
-                }
-            }
-        }
-        return prime;
+        System.out.println(prime);
     }
-    public static boolean calculate(int num) {
-        Scanner scanner = new Scanner(System.in);
-        boolean array = false;
-        int first, second, third, forth, fifth;
-        int test = 0, test1 = 0, test2 = 0, test3 = 0, test4 = 0;
-        int[] plindromArray = new int[5];
-        int[] tempPlindrome = new int[plindromArray.length];
-        int count = 1;
+    //------------------------------------------------------------------------------------------------------------------
+    public static void palindromeTest (){
+        System.out.println("Palindrome number test");
+        boolean palindrome = false;
+        int num = getIntInput();
+        int palindromeTest;
+        while (num < 10000 || num > 99999){
+            System.out.println("Enter a number that has exactly 5 numbers");
+            num = getIntInput();
+        }
+        palindromeTest = mirroredNumbers(num);
+        if (num == palindromeTest){
+            palindrome = true;
+        }
+        System.out.println(palindrome);
+    }
+    public static int mirroredNumbers (int num){
+        int newNum = 0;
         int tempNum = num;
-        for (int j = 0; j < count; ) {
-            first = (tempNum % 100000 - tempNum % 10000);
-            second = (tempNum % 10000 - tempNum % 1000);
-            third = (tempNum % 1000 - tempNum % 100);
-            forth = (tempNum % 100 - tempNum % 10);
-            fifth = (tempNum % 10);
-            test = first / 10000;
-            test1 = second / 1000;
-            test2 = third / 100;
-            test3 = forth / 10;
-            test4 = fifth;
-            plindromArray[0] = test;
-            plindromArray[1] = test1;
-            plindromArray[2] = test2;
-            plindromArray[3] = test3;
-            plindromArray[4] = test4;
-            for (int i = 0; i < 1; i++) {
-                first = (tempNum % 100000 - tempNum % 10000);
-                second = (tempNum % 10000 - tempNum % 1000);
-                third = (tempNum % 1000 - tempNum % 100);
-                forth = (tempNum % 100 - tempNum % 10);
-                fifth = (tempNum % 10);
-                tempPlindrome[4] = test;
-                tempPlindrome[3] = test1;
-                tempPlindrome[2] = test2;
-                tempPlindrome[1] = test3;
-                tempPlindrome[0] = test4;
-            }
-            if (plindromArray[4] == tempPlindrome[4]) {
-                System.out.println("the numbers entered are a plinrome");
-                j++;
-            }
-            if (plindromArray[4] != tempPlindrome[4]) {
-                System.out.println("the numbers are not a plindrome enter another 5 numbers!");
-                int num1 = scanner.nextInt();
-                tempNum = num1;
-            }
+        while (tempNum > 0){
+            int calculation = tempNum % 10;
+            newNum =  newNum * 10 + calculation;
+            tempNum /=  10;
         }
-        return array = true;
+        return newNum;
     }
-
     //------------------------------------------------------------------------------------------------------------------
 
     public static void printAllPrimeNumbersInRange(){
